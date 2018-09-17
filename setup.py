1c3f006d0b3c73dfd5ec015d9b59db87167a75c4
Fredrik Stromberg <fredrik314@gmail.com>
2018-09-17 12:01:07 +0100

    Added dotenv support for setting the environment

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='noncongruence_maass',
+    version='1.0',
+    packages=['noncong', 'noncong.maass', 'noncong.backend', 'noncong.subgroups'],
+    url='',
+    license='GPLv2',
+    author='fredrik',
+    author_email='fredrik314@gmail.com',
+    description='Computing with Noncongruence subgroups and Mass waveforms'
+)

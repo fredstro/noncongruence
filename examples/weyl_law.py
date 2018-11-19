e82ad4d70223c412e471eedcd76c3d8c0325ae17
Fredrik Stromberg <fredrik314@gmail.com>
2018-11-19 11:59:04 +0000

    updated

--- examples/weyl_law.py
+++ examples/weyl_law.py
@@ -265,7 +265,7 @@ class WeylsLaw(object):
         told = 0
         maxdiff = 0
         pts = [(T0, 0)]
-        for x in ScatteringDeterminant.objects.filter(group=g, sigma=0.5, t__gt=T0,t__lt=T + 1e-10).order_by('t'):
+        for x in ScatteringDeterminant.objects.filter(group=self.group, sigma=0.5, t__gt=T0,t__lt=T + 1e-10).order_by('t'):
             if self._verbose>1:
                 vstr = "{0:0>13.10f}".format(float(x.t))
                 sys.stdout.write("\r" + vstr)
